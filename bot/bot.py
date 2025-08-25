6c00052947f30c09e8ca8d3424f9577995c40eae
Ivan Yatsuk <ivankodpua@gmail.com>
2025-08-26 00:38:14 +0200

    Database connection

--- bot/bot.py
+++ bot/bot.py
@@ -2,12 +2,37 @@ import telebot
 import sqlite3
 
 
-API_TOKEN = "enter your token"
+API_TOKEN = "enter the bot token"
 
-connect = sqlite3.connect("email.db", check_same_thread=False)
-data = connect.cursor()
-data.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, email TEXT)")
-connect.commit()
+import sqlite3
+
+Connection = sqlite3.connect("sql.db")
+if Connection is None:
+    print("Connection failed")
+else:
+    cursor = Connection.cursor()
+    if cursor is None:
+        print("Cursor failed")
+    else:
+        print("DB Init")
+
+        query = "SELECT sqlite_version();"
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        if result:
+            print("SQLite Version is {}".format(result[0][0]))
+        else:
+            print("Fetch error")
+
+        # Закрываем курсор
+        cursor.close()
+
+    # Закрываем соединение
+    # Connection.close()
+     #print("SQLite Connection closed")
+
+
 
 
 bot = telebot.TeleBot(API_TOKEN)
@@ -16,7 +41,14 @@ bot = telebot.TeleBot(API_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Hello! I am the Mail-Checker Bot! You can add a email and I can to check if anyone has written to you. Use /help to see the commands.")
 
-@bot.message_handler(commands=['setemail'])
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Here is the list of the commands:")
+    bot.reply_to(message, "/setmail - enter your email."
+    "/sendmail - send the emails that you want to track.")
+
+
+@bot.message_handler(commands=['setmail'])
 def ask_email(message):
     bot.reply_to(message, "Write your email.")
 
